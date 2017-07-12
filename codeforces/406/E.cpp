bd652c726b5a51e4fde6b7898182b1e677af0215
becauseofyou <wuyiqi.splay@gmail.com>
2017-07-12 13:43:49 +0800

    add cf406_E.cpp test sublime git

---
+++ codeforces/406/E.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+const int  N = 100010;
+int a[N];
+
+int main () { 
+    int a,b;
+    while (scanf("%d%d",&a,&b) == 2 ) {
+    	printf("%d\n", a + b);
+    }
+    return 0;
+}
